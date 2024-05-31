bf60e85b3a626ad483453eee8caccc605d6e4acd
GabrielTalledo <gabriel_alejandro.tm@hotmail.com>
2024-05-30 20:43:07 -0500

    feat: partial develop

--- src/main/java/com/example/lab7_20211755/Controllers/WebServiceController.java
+++ src/main/java/com/example/lab7_20211755/Controllers/WebServiceController.java
@@ -5,6 +5,7 @@ import com.example.lab7_20211755.Entities.User;
 import com.example.lab7_20211755.Repositories.ResourceRepository;
 import com.example.lab7_20211755.Repositories.UserRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +26,7 @@ public class WebServiceController {
     //Listas auxiliares:
 
     ArrayList<Integer> listaRecursos = new ArrayList<>(List.of(5, 6, 7, 8));
+    ArrayList<String> listaTipo = new ArrayList<>(List.of("Contador", "Cliente", "AnalistaPromociones", "AnalistaLogistico"));
 
     // Métodos:
 
@@ -61,11 +63,16 @@ public class WebServiceController {
     }
 
     @PostMapping(value = {"/acceso"})
-    public ResponseEntity<HashMap<String, Object>> crearContador(@RequestParam(value = "userId") String userIdStr,
-                                                                 @RequestParam(value = "authorizedResource") String authorizedResourceStr) {
+    public ResponseEntity<HashMap<String, Object>> crearContador(@RequestParam(value = "userId", required = false) String userIdStr,
+                                                                 @RequestParam(value = "authorizedResource", required = false) String authorizedResourceStr) {
         HashMap<String, Object> respuesta = new HashMap<>();
         User user = new User();
         Resource resource = new Resource();
+
+        if(userIdStr == null || authorizedResourceStr== null){
+            respuesta.put("Mensaje","Debe incluir el id del usuario y el id del recurso.");
+            return ResponseEntity.badRequest().body(respuesta);
+        }
 
         HashMap<String,Integer> listaTipoRecurso = new HashMap<String,Integer>();
         listaTipoRecurso.put("Contador",5);
@@ -116,26 +123,123 @@ public class WebServiceController {
     }
 
     @GetMapping(value = {"/listado/{authorizedResource}"})
-    public ResponseEntity<HashMap<String, List<Object>>> listaUsuarios(@PathVariable("authorizedResource") String authorizedResourceStr){
+    public ResponseEntity<HashMap<String, List<Object>>> listaUsuarios(@PathVariable(value = "authorizedResource",required = false) String authorizedResourceStr){
 
         HashMap<String, List<Object>> respuesta = new HashMap<>();
+
+        HashMap<Integer,String> listaTipoRecurso = new HashMap<Integer,String>();
+        listaTipoRecurso.put(5,"Contador");
+        listaTipoRecurso.put(6,"Cliente");
+        listaTipoRecurso.put(7,"AnalistaPromociones");
+        listaTipoRecurso.put(8,"AnalistaLogistico");
+
+        if(authorizedResourceStr == null){
+            respuesta.put("Mensaje", Collections.singletonList("Debe incluir el id del usuario y el id del recurso."));
+            return ResponseEntity.badRequest().body(respuesta);
+        }
 
         try {
 
             int authorizedResource = Integer.parseInt(authorizedResourceStr);
 
             if(listaRecursos.contains(authorizedResource)){
+                Resource resource = resourceRepository.findById(authorizedResource).get();
 
-
-
+                respuesta.put("Recurso", Collections.singletonList(resource.getName()));
+                respuesta.put("Usuarios con acceso",userRepository.findAllByTypeEquals(listaTipoRecurso.get(authorizedResource)));
+                return ResponseEntity.ok(respuesta);
             }else{
                 respuesta.put("Mensaje", Collections.singletonList("El recurso ingresado no existe."));
                 return ResponseEntity.badRequest().body(respuesta);
             }
 
         }catch (NumberFormatException e){
-            respuesta.put("Mensaje", Collections.singletonList("El id del recurso deben ser número un número enteros positivo."));
+            respuesta.put("Mensaje", Collections.singletonList("El id del recurso debe ser número un número entero positivo."));
             return ResponseEntity.badRequest().body(respuesta);
         }
+    }
+
+    @DeleteMapping("/eliminar")
+    public ResponseEntity<HashMap<String, Object>> borrar(@RequestParam(value = "userId", required = false) String userIdStr){
+        HashMap<String, Object> respuesta = new HashMap<>();
+
+        if(userIdStr == null){
+            respuesta.put("Mensaje", Collections.singletonList("Debe incluir el id del usuario."));
+            return ResponseEntity.badRequest().body(respuesta);
+        }
+
+        try{
+            int id = Integer.parseInt(userIdStr);
+            Optional<User> byId = userRepository.findById(id);
+
+            if(byId.isPresent()){
+                userRepository.deleteById(id);
+                respuesta.put("Mensaje","El jugador fue eliminado exitosamente.");
+            }else{
+                respuesta.put("Mensaje","El ID enviado no existe.");
+                return ResponseEntity.badRequest().body(respuesta);
+            }
+
+            return ResponseEntity.ok(respuesta);
+        }catch (NumberFormatException e){
+            respuesta.put("Mensaje","El id un número entero positivo.");
+            return ResponseEntity.badRequest().body(respuesta);
+        }
+    }
+
+    //Actualizar tipo
+    @PutMapping(value = {"/actualizar"})
+    public ResponseEntity<HashMap<String, Object>> actualizar(@RequestParam(value = "userId", required = false) String userIdStr,
+    @RequestParam(value = "type", required = false) String type) {
+
+        HashMap<String, Object> respuesta = new HashMap<>();
+        User user = new User();
+
+        HashMap<String,Integer> listaTipoRecurso = new HashMap<String,Integer>();
+        listaTipoRecurso.put("Contador",5);
+        listaTipoRecurso.put("Cliente",6);
+        listaTipoRecurso.put("AnalistaPromociones",7);
+        listaTipoRecurso.put("AnalistaLogistico",5);
+
+        if(userIdStr == null){
+            respuesta.put("Mensaje","Debe incluir el id del usuario a eliminar y el tipo a cambiar.");
+            return ResponseEntity.badRequest().body(respuesta);
+        }
+
+        try {
+            int userId = Integer.parseInt(userIdStr);
+            int authorizedResourceNew = 0;
+
+            Optional<User> userOpt = userRepository.findById(userId);
+
+            //Validaciones:
+            boolean validacion = true;
+
+            if(userOpt.isPresent()){
+                user = userOpt.get();
+            }else{
+                respuesta.put("MensajeA","El id usuario no es válido.");
+                validacion = false;
+            }
+
+            if(!listaTipo.contains(type)){
+                respuesta.put("MensajeB","El tipo no es válido.");
+                validacion = false;
+            }
+
+            if(validacion){
+                authorizedResourceNew = listaTipoRecurso.get(type);
+                userRepository.cambiarTipoPorId(userId,type,authorizedResourceNew);
+                respuesta.put("Mensaje","Se cambió el tipo del usuario "+user.getName()+" a "+type+" exitosamente.");
+                return ResponseEntity.ok(respuesta);
+            }else{
+                return ResponseEntity.badRequest().body(respuesta);
+            }
+
+        }catch (NumberFormatException e){
+            respuesta.put("Mensaje","El id del usuario deben ser un número entero positivo.");
+            return ResponseEntity.badRequest().body(respuesta);
+        }
+
     }
 }
--- src/main/java/com/example/lab7_20211755/Repositories/UserRepository.java
+++ src/main/java/com/example/lab7_20211755/Repositories/UserRepository.java
@@ -1,9 +1,21 @@
 package com.example.lab7_20211755.Repositories;
 
 import com.example.lab7_20211755.Entities.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>  {
+
+    List<Object> findAllByTypeEquals(String type);
+
+    @Query(nativeQuery = true, value = "UPDATE users SET type = ?2, authorizedResource = ?3 WHERE userId = ?1")
+    @Modifying
+    @Transactional
+    void cambiarTipoPorId(int userId, String type, int resource);
 }
